6c5c5a72c927cb9aff6db588b8c3d4c73709a5cb
alejandra <alejandravidal3120@gmail.com>
2025-09-15 21:29:25 -0500

    Agregar proyecto C# con clases de arreglos, matrices y búsqueda de rutas

---
+++ OneDrive/Escritorio/arreglos-matrices-csharp-python/csharp/BusquedaRutasCSharp/Arreglos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusquedaRutasCSharp
+{
+    internal class Arreglos
+    {
+        public void Ejecutar()
+        {
+            int[] arreglo = { 1, 2, 3, 4, 5 };
+
+            // Multiplicar por índice
+            for (int i = 0; i < arreglo.Length; i++)
+                arreglo[i] = arreglo[i] * i;
+
+            Console.WriteLine("Después de multiplicar por índice:");
+            foreach (int val in arreglo)
+                Console.Write(val + " ");
+            Console.WriteLine();
+
+            // Búsqueda lineal
+            int valorABuscar = 0;
+            int posicion = -1;
+            for (int i = 0; i < arreglo.Length; i++)
+            {
+                if (arreglo[i] == valorABuscar)
+                {
+                    posicion = i;
+                    break;
+                }
+            }
+
+            if (posicion != -1)
+                Console.WriteLine($"Valor {valorABuscar} encontrado en posición {posicion}");
+            else
+                Console.WriteLine($"Valor {valorABuscar} no encontrado");
+        }
+    }
+}
---
+++ OneDrive/Escritorio/arreglos-matrices-csharp-python/csharp/BusquedaRutasCSharp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusquedaRutasCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Arreglos arreglos = new Arreglos();
+            arreglos.Ejecutar();
+
+            Matrices matrices = new Matrices();
+            matrices.Ejecutar();
+
+            BusquedaRutas busqueda = new BusquedaRutas();
+            busqueda.Ejecutar();
+
+            Console.WriteLine("Presiona una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
